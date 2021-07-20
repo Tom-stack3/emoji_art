4a495d787c185880aec9bc45db9df92f534bf063
Tom-stack3 <963010zaft@gmail.com>
2021-07-20 18:26:12 +0300

    added guinea

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from drawing import Drawing
 
 def main():
     d = Drawing()
-    d.translate_image("flags_try/cuba.jpg", True)
+    d.translate_image("flags_try/guinea.png", True)
     # d.add_text("Hi", "brown")
     #d.draw_amogus("pink")
     print(d)
