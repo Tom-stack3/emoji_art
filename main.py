40af18f82c4ce4658422129e7650a50ac5e35ca4
Tom-stack3 <963010zaft@gmail.com>
2021-07-20 18:33:50 +0300

    added discord

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from drawing import Drawing
 
 def main():
     d = Drawing()
-    d.translate_image("flags_try/guinea.png", True)
+    d.translate_image("flags_try/discord.jpg", True)
     # d.add_text("Hi", "brown")
     #d.draw_amogus("pink")
     print(d)
